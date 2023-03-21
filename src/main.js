2de00496e75e460b535262e118f6f1e399fe4bf1
CookedBear <121625084+CookedBear@users.noreply.github.com>
2023-03-21 17:14:22 +0800

    Update main.js for Process.vue

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/index.less'
+import draggable from "vuedraggable"
 // import './mock'
 
 const app = createApp(App)
