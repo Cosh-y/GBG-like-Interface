03ac80e1ec9713f8e00666e382a056397a403afe
ForeverYolo <91014282+ForeverYolo@users.noreply.github.com>
2023-03-27 17:41:26 +0800

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,9 @@ import Home from '@/views/Home.vue'
 import LayOut from '@/views/LayOut.vue'
 import Process from '@/views/Process.vue'
 import Run from '@/views/Run.vue'
+import Welcome from '@/views/Welcome.vue'
+import Welcome1 from '@/views/Welcome1.vue'
+import Welcome2 from '@/views/Welcome2.vue'
 import { Layout } from 'view-ui-plus'
 
 const routes = [
@@ -10,9 +13,24 @@ const routes = [
     path: '/',
     name: 'home',
     component: Home,
-    redirect: '/LayOut',
+    redirect: '/Welcome',
     // 子路由/嵌套路由
     children: [
+      {
+        path: '/Welcome1',
+        name: 'Welcome1',
+        component: Welcome1
+      },
+      {
+        path: '/Welcome2',
+        name: 'Welcome2',
+        component: Welcome2
+      },
+      {
+        path: '/Welcome',
+        name: 'Welcome',
+        component: Welcome
+      },
       {
         path: '/LayOut',
         name: 'LayOut',
