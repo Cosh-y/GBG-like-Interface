645a121f8f2278583f4eb89cba8c475523931678
ForeverYolo <91014282+ForeverYolo@users.noreply.github.com>
2023-03-28 00:56:20 +0800

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,8 @@ import Run from '@/views/Run.vue'
 import Welcome from '@/views/Welcome.vue'
 import Welcome1 from '@/views/Welcome1.vue'
 import Welcome2 from '@/views/Welcome2.vue'
+import LayOutToProcess from '@/views/LayOutToProcess'
+import Waiting from '@/views/Waiting'
 import { Layout } from 'view-ui-plus'
 
 const routes = [
@@ -17,34 +19,68 @@ const routes = [
     // 子路由/嵌套路由
     children: [
       {
+        path: '/LayOutToProcess',
+        name: 'LayOutToProcess',
+        component: LayOutToProcess,
+        meta: {
+          index: 8,
+        },
+      },
+      {
+        path: '/Waiting',
+        name: 'Waiting',
+        component: Waiting,
+        meta: {
+          index: 5,
+        },
+      },
+      {
         path: '/Welcome1',
         name: 'Welcome1',
-        component: Welcome1
+        component: Welcome1,
+        meta: {
+          index: 2,
+        },
       },
       {
         path: '/Welcome2',
         name: 'Welcome2',
-        component: Welcome2
+        component: Welcome2,
+        meta: {
+          index: 2,
+        },
       },
       {
         path: '/Welcome',
         name: 'Welcome',
-        component: Welcome
+        component: Welcome,
+        meta: {
+          index: 1,
+        },
       },
       {
         path: '/LayOut',
         name: 'LayOut',
-        component: LayOut
+        component: LayOut,
+        meta: {
+          index: 3,
+        },
       },
       {
         path: '/Process',
         name: 'Process',
-        component: Process
+        component: Process,
+        meta: {
+          index: 4,
+        },
       },
       {
         path: '/Run',
         name: 'Run',
-        component: Run
+        component: Run,
+        meta: {
+          index: 6,
+        },
       }
     ]
   },
